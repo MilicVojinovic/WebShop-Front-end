3e38ddf47b518354235cb65f64355d3eb97817c6
Milic Vojinovic <milic.vojinovic@gmail.com>
2020-08-13 13:19:48 +0200

    Add the hash router mechanism to the application

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Nav, Container } from "react-bootstrap";
+import { HashRouter, Link } from "react-router-dom";
 
 export class MainMenuItem {
     text: string = '';
@@ -29,15 +30,15 @@ export class MainMenu extends React.Component<MainMenuProperties> {
     constructor(props: Readonly<MainMenuProperties>) {
         super(props);
 
-        this.state= {
+        this.state = {
             items: props.items,
         };
 
-        setInterval(() => {
-            const novaLista  = [... this.state.items];
-            novaLista.push(new MainMenuItem("Naslov" , "/link"));
-            this.setItems(novaLista);
-        }, 2000);
+        // setInterval(() => {
+        //     const novaLista = [...this.state.items];
+        //     novaLista.push(new MainMenuItem("Naslov", "/link"));
+        //     this.setItems(novaLista);
+        // }, 2000);
     }
 
     setItems(items: MainMenuItem[]) {
@@ -50,9 +51,9 @@ export class MainMenu extends React.Component<MainMenuProperties> {
         return (
             <Container>
                 <Nav variant="tabs">
-                    {
-                        this.state.items.map(this.makeNavLink)
-                    }
+                    <HashRouter>
+                        {this.state.items.map(this.makeNavLink)}
+                    </HashRouter>
                 </Nav>
             </Container>
         );
@@ -61,9 +62,10 @@ export class MainMenu extends React.Component<MainMenuProperties> {
 
     private makeNavLink(item: MainMenuItem) {
         return (
-            <Nav.Link href={item.link}>
+            <Link to= {item.link} className="nav-link">
                 {item.text}
-            </Nav.Link>
+            </Link>
+
         );
     }
 }
