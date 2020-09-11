5fbe4d98971a4daa9c5452e16bb75699cd86e086
Milic Vojinovic <milic.vojinovic@gmail.com>
2020-09-11 13:54:40 +0200

    Implement article feature filter

--- src/components/CategoryPage/CategoryPage.tsx
+++ src/components/CategoryPage/CategoryPage.tsx
@@ -27,8 +27,16 @@ interface CategoryPageState {
         priceMinimum: number;
         priceMaximum: number;
         order: "name asc" | "name desc" | "price asc" | "price desc";
-
+        selectedFeatures: {
+            featureId: number;
+            value: string;
+        }[]
     };
+    features: {
+        featureId: number;
+        name: string;
+        values: string[];
+    }[];
 }
 
 // data that we are getting from backend server api for subcategory
@@ -66,8 +74,16 @@ export default class CategoryPage extends React.Component<CategoryPageProperties
                 priceMinimum: 0.01,
                 priceMaximum: 100000,
                 order: "price asc",
-            }
+                selectedFeatures: [],
+            },
+            features: [],
         };
+    }
+
+    private setFeatures(features: any) {
+        this.setState(Object.assign(this.state, {
+            features: features
+        }));
     }
 
     private setLoginState(isUserLoggedIn: boolean) {
@@ -170,7 +186,49 @@ export default class CategoryPage extends React.Component<CategoryPageProperties
 
     }
 
-    // for Button event function no need to pass event as argument 
+
+    private featuresFilterChanged(event: React.ChangeEvent<HTMLInputElement>) {
+        const featureId = Number(event.target.dataset.featureId);
+        const value = event.target.value;
+
+        if (event.target.checked) {
+            this.addFeatureFilterValue(featureId, value)
+        } else {
+            this.removeFeatureFilterValue(featureId, value)
+        }
+    }
+
+    private addFeatureFilterValue(featureId: number, value: string) {
+        const newSelectedFeatures = [...this.state.filters.selectedFeatures];
+        newSelectedFeatures.push({
+            featureId: featureId,
+            value: value,
+        });
+
+        this.setSelectedFeatures(newSelectedFeatures);
+
+    }
+
+    private removeFeatureFilterValue(featureId: number, value: string) {
+        const newSelectedFeatures = this.state.filters.selectedFeatures.filter(
+            record => {
+                return !(record.featureId === featureId && record.value === value);
+            });
+
+        this.setSelectedFeatures(newSelectedFeatures);
+    }
+
+
+    private setSelectedFeatures(newSelectedFeatures: { featureId: number; value: string; }[]) {
+        this.setState(Object.assign(this.state, {
+            filters: Object.assign(this.state.filters, {
+                selectedFeatures: newSelectedFeatures
+            })
+        }));
+
+        console.log(this.state);
+    }
+
     private applyFilters() {
         this.getCategoryData();
     }
@@ -224,18 +282,39 @@ export default class CategoryPage extends React.Component<CategoryPageProperties
                     </Form.Control>
                 </Form.Group>
 
+                {this.state.features.map(this.printFeatureFilterComponent, this)}
+
                 <Form.Group>
                     <Button variant="primary" block
                         onClick={() => this.applyFilters()}>
                         <FontAwesomeIcon icon={faSearch} /> Search
                     </Button>
                 </Form.Group>
-
-
-
             </>
         )
     }
+
+    private printFeatureFilterComponent(feature: { featureId: number; name: string; values: string[]; }) {
+        return (
+            <Form.Group>
+                <Form.Label>
+                    <strong>{feature.name}</strong>
+                </Form.Label>
+                {feature.values.map(value =>
+                    this.printFeatureFilterCheckBox(feature, value), this)}
+            </Form.Group>
+        );
+    }
+
+    private printFeatureFilterCheckBox(feature: any, value: string) {
+        return (
+            <Form.Check type="checkbox" label={value}
+                value={value}
+                data-feature-id={feature.featureId}
+                onChange={(e: any) => this.featuresFilterChanged(e as any)} />
+        );
+    }
+
 
     private printOptionalMessage() {
         if (this.state.message === '') {
@@ -375,15 +454,43 @@ export default class CategoryPage extends React.Component<CategoryPageProperties
         // search articles
 
         const orderParts = this.state.filters.order.split(' ');
-            const orderBy = orderParts[0];
-            const orderDirection = orderParts[1].toUpperCase();
+        const orderBy = orderParts[0];
+        const orderDirection = orderParts[1].toUpperCase();
+
+
+        const featureFilters: any[] = [];
+
+        for (const item of this.state.filters.selectedFeatures) {
+            let found = false;
+            let foundRef = null;
+
+            for (const featureFilter of featureFilters) {
+
+                if (featureFilter.featureId === item.featureId) {
+                    found = true;
+                    foundRef = featureFilter;
+                    break;
+                }
+            }
+
+            if (!found) {
+                featureFilters.push({
+                    featureId : item.featureId,
+                    values : [item.value]
+                });
+            } else {
+                foundRef.values.push(item.value);
+            }
+
+
+        }
 
         api('api/article/search', 'post', {
             categoryId: Number(this.props.match.params.cId),
             keywords: this.state.filters.keywords,
             priceMin: this.state.filters.priceMinimum,
             priceMax: this.state.filters.priceMaximum,
-            features: [],
+            features: featureFilters,
             orderBy: orderBy,
             orderDirection: orderDirection
         })
@@ -430,7 +537,27 @@ export default class CategoryPage extends React.Component<CategoryPageProperties
                 this.setArticles(articles);
 
             });
+
+        this.getFeatures();
     };
+
+    private getFeatures() {
+        api('api/feature/values/' + this.props.match.params.cId, 'get', {})
+            .then((res: ApiResponse) => {
+
+                if (res.status === 'login') {
+                    return this.setLoginState(false);
+                }
+
+                if (res.status === 'error') {
+                    return this.setMessage('Request error.Please try to refresh the page.');
+                }
+
+                this.setFeatures(res.data.features)
+
+            });
+
+    }
 }
 
 
